ad38562f4249594d39b2dc4ae49dddd3465cbb2b
SBahrase <Sina.Bahrasemani@cern.ch>
2018-05-22 22:06:53 -0700

    adding parallel processing utilities

---
+++ hpana/cluster/setup_batch.sh
@@ -0,0 +1,8 @@
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh --quiet
+
+# ROOT
+source $ATLAS_LOCAL_ROOT_BASE/packageSetups/atlasLocalROOTSetup.sh --quiet
+
+

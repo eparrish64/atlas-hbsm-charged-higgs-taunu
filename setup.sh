6e71a7aa9afe53defb8eab0ae9f5a3c4c86277e6
SBahrase <Sina.Bahrasemani@cern.ch>
2018-09-19 18:25:27 -0700

    re-designing the package strcture according to a standard Python Package layout

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 
+if [ $# -lt 1 ]; then
+    echo "Pass the virtual env path plz"
+	return
+fi
+
+## ATLAS
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh --quiet
 
-# ROOT
+## ROOT
+echo "Installing ROOT ..."
 source $ATLAS_LOCAL_ROOT_BASE/packageSetups/atlasLocalROOTSetup.sh --quiet
 
-# activate the virtual env
-source /cluster/warehouse/sbahrase/PythonPackages/VirtualEnvs/Hplus/bin/activate
+## activate the virtual env
+source "$1"/bin/activate
+
+## setup hpana
+"$1"/bin/pip install -e .

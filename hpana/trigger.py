2582ace18d473ae20310c32fdaf6e527fb10fd59
hbaluchb <Sina.Bahrasemani@cern.ch>
2018-11-20 11:03:40 -0800

    updating 2016 lep trigger

--- hpana/trigger.py
+++ hpana/trigger.py
@@ -37,7 +37,7 @@ TRIGGERS = {
             "2015": ("((run_number <= 288000)&&"\
                      "(HLT_e24_lhmedium_L1EM20VH || HLT_e60_lhmedium || HLT_e120_lhloose || HLT_mu20_iloose_L1MU15 || HLT_mu50))"), 
             "2016": ("((run_number > 288000)"\
-                     "&&(HLT_e26_lhtight_nod0_ivarloose || HLT_e60_lhmedium_nod0 || HLT_e140_lhloose_nod0 || HLT_mu26_ivarmedium || HLT_mu50))"),
+                     "&&(HLT_e26_lhtight_nod0_ivarloose || HLT_e60_lhmedium_nod0 || HLT_e140_lhloose_nod0 ||HLT_mu24_ivarloose||HLT_mu24_ivarmedium || HLT_mu26_ivarmedium || HLT_mu50))"),
             "2017": ("(HLT_e26_lhtight_nod0_ivarloose || HLT_e60_lhmedium_nod0 || HLT_e140_lhloose_nod0 || HLT_mu26_ivarmedium || HLT_mu50)"),
             "2018": ""
         },

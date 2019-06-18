87905c0250d790e52e72f2b48900e292cc93aca6
Blake Burghgrave <blake.burghgrave@cern.ch>
2019-06-18 02:45:44 +0200

    fill_scores_mult to loop over trees once regardless of how many mass points there are

--- hpana/mva/evaluation.py
+++ hpana/mva/evaluation.py
@@ -448,6 +448,93 @@ def fill_scores_histogram(tree, models, hist_template=None, event_selection=None
     # End loop over models
 
 ##-----------------------------------------------
+## 
+##-----------------------------------------------
+def fill_scores_mult(tree, all_models, hist_templates, event_list, event_weight=None, correct_upsilon=False):
+    """ evaluate scores from a model on a tree and fill a histogram
+    Parameters
+    ----------
+    ...
+
+    Return
+    ------
+    ...
+    """
+
+    #if log.isEnabledFor(logging.DEBUG):
+    #    # Converting these to strings is slow, even if the logger doesn't print anything
+    #    log.debug("---------------- models:\n %r"%models)
+
+    event_number = ROOT.TTreeFormula("event_number", "event_number", tree)
+
+    clf_feats_tf = dict()
+    for mtag in all_models:
+      for feat in all_models[mtag][0].features:
+          if feat.name in clf_feats_tf: continue
+          if correct_upsilon and "upsilon" in feat.name.lower():
+              clf_feats_tf[feat.name] = ROOT.TTreeFormula(feat.name, QCD.UPSILON_CORRECTED["mc16"], tree)
+          else:
+              clf_feats_tf[feat.name] = ROOT.TTreeFormula(feat.name, feat.tformula, tree)
+      for f_tf in clf_feats_tf.values():
+          f_tf.SetQuickLoad(True)
+
+    ## - - cache Tree
+    tree.SetCacheSize(32*2**20)
+    tree.SetCacheLearnEntries()
+    infos = dict()
+    for mtag in all_models:
+      infos[mtag] = dict() # Cache of features for events passing selection, indexed by folds    
+      for model in all_models[mtag]:
+          if model.kfolds not in infos[mtag]: infos[mtag][model.kfolds] = dict()
+          if model.fold_num not in infos[mtag][model.kfolds]: infos[mtag][model.kfolds][model.fold_num] = [[], []] # Features and weights
+    entries = []
+    for entry in xrange(event_list.GetN()):
+      entries.append(event_list.GetEntry(entry))
+    for entry in entries:
+
+        tree.LoadTree(entry)
+
+        # Logging output
+        #if (entry%10000==0): 
+        #    log.info("Tree: {0}, Event: {1}/{2}".format(tree.GetName(), entry+1, ents))
+
+        ## - - evaluate features
+        feats = { n:v.EvalInstance() for n,v in clf_feats_tf.iteritems() }
+        weight = event_weight.EvalInstance()
+        eventnum = event_number.EvalInstance()
+
+        ## - - build features vectors per fold
+        for mtag in all_models:
+          event_feats = [feats[feat.name] for feat in all_models[mtag][0].features]
+          for kfolds in infos[mtag]:
+              for fold in infos[mtag][kfolds]:
+                  if eventnum % kfolds == fold:
+                      infos[mtag][kfolds][fold][0].append(event_feats)
+                      infos[mtag][kfolds][fold][1].append(weight)
+    # End loop over entries
+
+    ## - - convert to np.array
+    for mtag in infos:
+      for kfolds in infos[mtag]:
+          for fold in infos[mtag][kfolds]:
+              if len(infos[mtag][kfolds][fold][0]) > 0:
+                  old = infos[mtag][kfolds][fold]
+                  infos[mtag][kfolds][fold][0] = np.array(infos[mtag][kfolds][fold][0])
+
+    for mtag in all_models:
+      for model in all_models[mtag]:
+          # Loop over models, evaluating events and filling trees
+          # In theory we could do this periodically while looping over events, if memory becomes a problem
+          events = infos[mtag][model.kfolds][model.fold_num]
+          if len(events[0]) == 0: continue # No events passed the selection
+          scores = model.predict_proba(events[0])
+          for idx in xrange(len(scores)):
+              hist_templates[mtag].Fill(scores[idx][1], events[1][idx]) # <! probability of belonging to class 1 (SIGNAL)
+              if idx%100000==0:
+                  log.debug("%r : %r "%(events[0][idx], scores[idx][1]))
+    # End loop over models
+
+##-----------------------------------------------
 ##
 ##-----------------------------------------------
 def evaluate_scores_on_trees(file_name, models, features=[], backend="sklearn"):

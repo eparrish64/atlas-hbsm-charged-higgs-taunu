a881ff6a753e0c4479ec1447a89e0322e90b266d
Elliot Parrish <elliot.wesley.parrish@cern.ch>
2021-04-12 18:12:03 +0000

    Fixed the help messages for outdir and submitdir

--- hpana/cmd.py
+++ hpana/cmd.py
@@ -85,10 +85,10 @@ def get_ana_parser(base_parser=None):
                             help="merge histograms")
 
     ana_parser.add_argument("--outdir", type=str, default="outdir",
-                            help="where to put the job scripts. You will launch condor jobs from here.")
+                            help="Where to put the processed histograms")
 
     ana_parser.add_argument("--submitdir", type=str,
-                            help="Where to put the processed histograms")
+                            help="where to put the job scripts. You will launch condor jobs from here.")
 
     ana_parser.add_argument("--cluster", action="store_true",
                             help="if you wnat to submit jobs to the cluster")

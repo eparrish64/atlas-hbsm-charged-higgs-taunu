b3e960c451d8398d719af44a6ca00e19706ffebc
eparrish <elliot.wesley.parrish@cern.ch>
2021-09-21 16:57:23 +0200

    changed back to run-analysis friendly code. got rid of hack for cutflows

--- hpana/dataset_hists.py
+++ hpana/dataset_hists.py
@@ -127,10 +127,10 @@ def dataset_hists(hist_worker,
                     if syst_type == "WEIGHT":
                         if isinstance(syst_var.title, dict):
                             ######
-                            # sw = syst_var.title[category.name][0]
+                            sw = syst_var.title[category.name][0]
                             # I do not like this fix. This works for cutflows, but I doubt it will work for run-analysis
                             ######
-                            sw = syst_var.title["SR_%s"%(channel.upper())][0]
+                            # sw = syst_var.title["SR_%s"%(channel.upper())][0]
                             # sw = syst_var.title["SR_TAUJET"][0]
                         else:
                             sw = syst_var.title
